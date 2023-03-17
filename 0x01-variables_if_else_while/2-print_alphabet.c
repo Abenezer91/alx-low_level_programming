37022fa5636393f36c42860e423d8727dca9db80
Abenezer91 <alxabenezer@gmail.com>
2023-03-17 09:17:46 -0700

    I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 26; ++i)
 	{
-	    putchar(alp[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
