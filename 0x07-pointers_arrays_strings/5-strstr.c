5da321563e11fee4c30b916e1f00f62d5cf8c84b
Abenezer91 <alxabenezer@gmail.com>
2023-04-04 02:20:12 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *_strstr - Entry point
+  *@haystack: input
+  *@needle: input
+  *Return: Returns a pointer to the beginning of the located substring
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *r = haystack;
+		char *d =needle;
+
+		while (*r == *d && *d != '\0')
+		{
+			r++;
+			d++;
+		}
+
+		if (*d == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
