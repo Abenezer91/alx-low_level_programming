0c5ac8d84b4eb29ea4f9a0b96512e8664ab5ced0
Abenezer91 <alxabenezer@gmail.com>
2023-04-04 03:25:52 -0700

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_diagrams - Entry point
+  *@a: input
+  *@size: input
+  *Return: to make sure you understand how an array of array is stored in memory
+  */
+void print_diagsums(int *a, int size)
+{
+	int num1, num2, p;
+
+	num1 = 0;
+	num2 = 0;
+
+	for (p = 0; p < size; p++)
+	{
+		num1 = num1 + a[p* size + p];
+	}
+
+	for (p = size - 1; p >= 0; p--)
+	{
+		num2 += a[p* size + (size - p - 1)];
+	}
+	printf("%d, %d\n", num1, num2);
+}
+
