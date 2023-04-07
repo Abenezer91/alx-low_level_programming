f8da570ba9ce844abb5602d9370bee21b3787b48
Abenezer91 <alxabenezer@gmail.com>
2023-04-07 04:15:00 -0700

    It is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - prints the name of the program 
+  *@argc: number of arguments 
+  *@argv: array of arguments 
+  *
+  *Return: You should not remove the path
+  */
+int main (int argc _attribute _((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
