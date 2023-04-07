85ed2486ced52a175e1bb617e858e82a3933abdb
Abenezer91 <alxabenezer@gmail.com>
2023-04-07 04:43:43 -0700

    what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
-  *main - prints the name of the program 
-  *@argc: number of arguments 
-  *@argv: array of arguments 
+  *main - prints the name of the program
+  *@argc: number of arguments
+  *@argv: array of arguments
   *
   *Return: You should not remove the path
   */
