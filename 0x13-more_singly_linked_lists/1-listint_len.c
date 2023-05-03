84fb414a3ec7b6e7c2c2f416a5ea2d5d81cd2a31
Abenezer91 <alxabenezer@gmail.com>
2023-05-03 09:06:02 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - input 
+ * @h: linked list_input
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
